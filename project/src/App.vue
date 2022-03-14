122f2c99df3c1bb40e69c881ee91f256557308ad
romainmltr <romain_malaterre@hotmail.fr>
2022-03-14 17:05:32 +0100

    ad SASS

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,26 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Page1 from './components/Page1.vue';
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+<template>
+  <Page1></Page1>
+</template>
+
+<style></style>
---
+++ project/src/components/Page1.vue
@@ -0,0 +1,21 @@
+<template>
+
+<section class='test'>
+<p> lorem ipsum dolor sit amet, consectetur adipiscing elit et just aliquet et just al 
+</p>
+<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/langfr-220px-Vue.js_Logo_2.svg.png'>
+</section>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
